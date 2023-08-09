71e454498343af1025693dde91250c583f9fe056
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2023-08-09 18:00:06 +0800

    Able to download all songs from bandori

--- bushiroad/bushiroad/spiders/bandori_spider.py
+++ bushiroad/bushiroad/spiders/bandori_spider.py
@@ -38,13 +38,14 @@ for name in expected_band_names_of_category_page:
 class BandoriSpider(scrapy.Spider):
     name = "bandori"
     song_dl_count = 0
-    songs_saved = 0
+    songs_downloaded_and_saved = 0
     songs_found = 0
     start_urls = [scheme + subdo + sld + tld + subdi + category for category in song_categories_url_encoded]
 
     def parse(self, response: Response):
         category = self.get_song_category_of_category_page(response=response)
         band_names = self.get_band_names_of_category_page(response=response, category=category)
+        # for each band, yield a request to each song link
         for band_name in band_names:
             song_links = self.get_song_links_for_band_of_category_page(response=response, band_name=band_name, category=category)
             for song_link in song_links:
@@ -53,25 +54,29 @@ class BandoriSpider(scrapy.Spider):
                 yield scrapy.Request(url, self.parse_song_page, cb_kwargs=cb_kwargs)
 
     def parse_song_page(self, response: Response, category: str, band_name: str):
-        dl_links = self.get_dl_links_for_song_page(response=response, category=category, band_name=band_name)
-        for dl_link in dl_links:
-            cb_kwargs={'category': category, 'band_name': band_name}
-            yield scrapy.Request(dl_link, self.parse_song_download_link, cb_kwargs=cb_kwargs)
+        dl_links = self.get_dl_links_of_song_page(response=response, category=category, band_name=band_name)
+        file_names = self.get_file_names_of_dl_links(response=response, dl_links=dl_links) # url-decoded
+        file_names = [clean(re.match(r'(.+)(\.[^.]+)', file_name).group(1).rstrip('.') + re.match(r'(.+)(\.[^.]+)', file_name).group(2)) for file_name in file_names] # trailing dots stripped then cleaned
+        folder_name = self.get_folder_name(response=response, file_names=file_names) # folder_name is shortest file_name with extension removed 
+        file_dir_path = f'{songs_dir}/{clean(band_name)}/{category}/{clean(folder_name)}'
+        os.makedirs(file_dir_path, exist_ok=True)
+        for (dl_link, file_name) in zip(dl_links, file_names, strict=True):
+            file_path = os.path.join(file_dir_path, file_name)
+            if not os.path.exists(file_path):
+                cb_kwargs={'category': category, 'band_name': band_name, 'file_path': file_path}
+                yield scrapy.Request(dl_link, self.parse_song_download_link, cb_kwargs=cb_kwargs)
 
-    def parse_song_download_link(self, response: Response, category: str, band_name: str):
+    def parse_song_download_link(self, response: Response, category: str, band_name: str, file_path: str):
         content_type = response.headers.get('Content-Type').decode('utf-8')
         if 'ogg' in content_type:
-            file_name_url_encoded = re.match(r'^(https://static\.wikia\.nocookie\.net/bandori/images/[^/]+/[^/]+/)([^/]+)', response.url).group(2)
-            file_name = urllib.parse.unquote(file_name_url_encoded)
-            file_path = f'{songs_dir}/{clean(band_name)}/{category}/{clean(file_name)}'
-            if not os.path.exists(file_path):
-                with open(file_path, 'wb') as music_file:
-                    music_file.write(response.body)
-                self.songs_saved += 1
+            with open(file_path, 'wb') as music_file:
+                music_file.write(response.body)
+            self.songs_downloaded_and_saved += 1
         else:
             self.logger.error(f'Unrecognized content type!')
     
     def get_song_category_of_category_page(self, response: Response):
+        # original_songs, etc
         category: str = response.url.replace('https://bandori.fandom.com/wiki/Category:', '')
         if category not in song_categories_url_encoded:
             self.logger.error(f'Unexpected category {category} extracted from category page url at {response.url}')
@@ -91,6 +96,7 @@ class BandoriSpider(scrapy.Spider):
             raise ValueError(f'Could not find any bands from {category} category page at {response.url}')
 
     def get_song_links_for_band_of_category_page(self, response: Response, band_name: str, category: str):
+        # black shout, louder, etc for roselia; kizuna music, popping dream, etc for poppin'party
         links = response.xpath(f'//td[text()="{band_name}"]/following-sibling::td[position()=1]/div/a/@href').getall()
         if len(links) > 0:
             self.logger.info(f'Parsing {len(links)} {category} song links for {band_name}')
@@ -100,12 +106,12 @@ class BandoriSpider(scrapy.Spider):
             self.logger.error(f'Could not find any songs for {band_name} from {category} category page at {response.url}')
             raise ValueError(f'Could not find any songs for {band_name} from {category} category page at {response.url}')
 
-    def get_dl_links_for_song_page(self, response: Response, category: str, band_name: str):
+    def get_dl_links_of_song_page(self, response: Response, category: str, band_name: str):
         xpaths = ['//span[@id="Audio"]/parent::h2/following-sibling::table[position()=1][@class="article-table"]/tbody/tr[position()>1]/td/center/audio/@src',
                   '//span[@id="Audio"]/parent::h2/following-sibling::div[position()=1][@class="tabber wds-tabber"]/div[position()>1]/table/tbody/tr[position()>1]/td/center/audio/@src',
                   '//span[@id="Audios"]/parent::h2/following-sibling::div[position()=1][@class="tabber wds-tabber"]/div[position()>1]/table/tbody/tr[position()>1]/td/center/audio/@src',
                   '//span[@id="Audios"]/parent::h2/following-sibling::table[position()=1]/tbody/tr[position()>1]/td/center/audio/@src']
-        dl_links: list[str] = []
+        dl_links = []
         for xpath in xpaths:
             dl_links = response.xpath(xpath).getall()
             dl_links_length = len(dl_links)
@@ -115,7 +121,27 @@ class BandoriSpider(scrapy.Spider):
         self.logger.error(f'Unable to get download links from song page {response.url} of {band_name} {category}')
         return dl_links
     
+    def get_file_names_of_dl_links(self, response: Response, dl_links: list[str]):
+        file_names = [urllib.parse.unquote(re.match(r'^(https://static\.wikia\.nocookie\.net/bandori/images/[^/]+/[^/]+/)([^/]+)', dl_link).group(2)) for dl_link in dl_links]
+        file_names_length = len(file_names)
+        dl_links_length = len(dl_links)
+        if file_names_length != dl_links_length:
+            self.logger.error(f'Retrieved {file_names_length} file names although there are {dl_links_length} dl_links at {response.url}')
+            raise ValueError(f'Retrieved {file_names_length} file names although there are {dl_links_length} dl_links at {response.url}')
+        return file_names
+    
+    def get_folder_name(self, response: Response, file_names: list[str]):
+        if len(file_names) != 0:
+            folder_name = file_names[0]
+            for file_name in file_names:
+                if len(file_name) < len(folder_name):
+                    folder_name = file_name
+            folder_name = re.match(r'(.+)(\.[^.]+)', folder_name).group(1)
+            return folder_name
+        else: 
+            return ''
+    
     def closed(self, reason):
         self.logger.info(f'Songs found: {self.songs_found}')
         self.logger.info(f'Song download links found: {self.song_dl_count}')
-        self.logger.info(f'Links downloaded from and saved: {self.songs_saved}')
+        self.logger.info(f'Links downloaded from and saved: {self.songs_downloaded_and_saved}')

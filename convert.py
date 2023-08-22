bf1c732048e7a60069f5ae940af83c851ad903bc
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2023-08-22 18:10:06 +0800

    added comments for clarity

--- convert.py
+++ convert.py
@@ -3,21 +3,20 @@ import os
 import logging
 import datetime
 
-# create formatter
-formatter = logging.Formatter(u'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# create handler
+formatter = logging.Formatter(
+    u'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log_path = os.path.join('convert_logs', f'convert_{datetime.date.today()}.log')
-handler = logging.FileHandler(log_path, encoding='utf8') 
+handler = logging.FileHandler(log_path, encoding='utf8')
 handler.setFormatter(formatter)
-
-# create logger
 logger = logging.getLogger('convert')
 logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+
 
 converted = 0
 files_scanned = 0
+
 
 dir_walk = os.path.join('bushiroad', 'songs')
 for root, d_names, f_names in os.walk(dir_walk):
@@ -25,17 +24,21 @@ for root, d_names, f_names in os.walk(dir_walk):
         f_name = f_names[0]
         files_scanned += 1
         f_path = os.path.join(root, f_name)
-        ogg = AudioSegment.from_ogg(f_path)
         (f_path_no_ext, ext) = os.path.splitext(f_path)
-        (band_name, category) = root.split(os.sep)[2:4]
-        new_f_path = os.path.split(f_path)[0].replace(dir_walk, os.path.join('output', 'base_songs_mp3')) + f' {band_name}_{category}' + '.mp3'
+        (band_name, category) = root.split(os.sep)[
+            2:4]  # f'bushiroad/songs/{band_name}/{song_category}/{song_name}/{song_name}_{song_type}.ogg'
+        new_f_path = os.path.split(f_path)[0].replace(dir_walk, os.path.join(
+            'output', 'base_songs_mp3')) + f' {band_name}_{category}' + '.mp3'  # f'output/base_songs_mp3/{band_name}/{song_category}/{song_name} {band_name}_{song_category}'
         if not os.path.exists(new_f_path):
+            ogg = AudioSegment.from_ogg(f_path)
             os.makedirs(os.path.split(new_f_path)[0], exist_ok=True)
             logger.info(f'Converted {f_name} and saved it to {new_f_path}')
             ogg.export(new_f_path, format='mp3')
             converted += 1
         else:
-            logger.debug(f'Did not convert {f_name} as it already exists at {new_f_path}')
+            logger.debug(
+                f'Did not convert {f_name} as it already exists at {new_f_path}')
+
 
 logger.info(f'Total songs found: {files_scanned}')
 logger.info(f'Total songs converted: {converted}')
